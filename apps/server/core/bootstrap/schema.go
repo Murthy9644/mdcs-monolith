4686d440dd81261b1f26b6d0a62786c7cd13818e
Satyanarayana Murthy <bvjsmurthy90@gmail.com>
2026-05-21 12:47:27 +0530

    feat(database): add schemaCheck function scaffold for schema validatio.
    Implement schemaCheck function to verify file existence and handle missing files.

--- apps/server/core/bootstrap/schema.go
+++ apps/server/core/bootstrap/schema.go
@@ -1,4 +1,50 @@
 package bootstrap
 
+import {
+    "os"
+    "models" // need to be resolved
+}
+
 // Generate and maintain the files in database.
 // Used for v1 where data is stored locally on the server
+
+/*
+During schema phase of bootstrap,
+phase 1: check schema existence
+Check if the required files already exist or not
+
+phase 2: file creation
+If the required files are missing, create default files
+
+phase 3: schema and format validation
+- If invalid data items are present, update with default for that field only
+- If the file format (JSON in this case) is incorrect, replace with a default
+file
+*/
+
+func schemaCheck() bool{
+    var files map[string]string = {
+        "users": "disk/users.json",
+        "workspaces": "disk/workspaces.json",
+        "devices": "disk/devices.json"
+    }
+
+    for file, path := range files{
+        _, err := os.Stat(path)
+
+        if os.IsNotExist(err){
+            switch key{
+                case "users":
+                    //
+
+                case "workspaces":
+                    //
+
+                case "devices":
+                    //
+            }
+        }
+    }
+
+    return true
+}
