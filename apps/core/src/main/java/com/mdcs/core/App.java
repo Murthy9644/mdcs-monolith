236836cbd8eb87a952275961bdb7a4d765b6abca
Murthy9644 <bvjsmurthy90@gmail.com>
2026-08-01 14:45:02 +0530

    feat(ipc): implement IPC writer and async reader with send and request functionalities

--- apps/core/src/main/java/com/mdcs/core/App.java
+++ apps/core/src/main/java/com/mdcs/core/App.java
@@ -16,8 +16,6 @@ This implementation has some tweaks on the previous Report and Envelop mechanism
     - Completely removed JobType and Summary. Manager doesn't need to know master - worker
     architecture of Core.
 
-Stream logging structure: [type][length]<payload>;
-
 Point to be noted is, this stream is just a communication between Core and Manager, and the logs
 are not for UI purpose but mainly for internal use. UI module will decide what/how to show.
 */
--- apps/core/src/main/java/com/mdcs/core/Stream.java
+++ apps/core/src/main/java/com/mdcs/core/Stream.java
@@ -1,5 +1,16 @@
 package com.mdcs.core;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /*
  * Architecture note:
  *
@@ -20,60 +31,186 @@ package com.mdcs.core;
  *
  * This class is responsible only for serializing/deserializing the protocol, not for implementing
  * any business or UI logic.
+ * 
+ * Stream logging structure: "[id][type][length]<payload>"
  */
-
-import java.io.PrintStream;
-import java.util.Scanner;
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.LinkedBlockingDeque;
-
-class IPCWriter implements Runnable{
-    private BlockingDeque<String> ipc_queue;
-
-    @Override
-    public void run(){
-        //
-    }
-
-    IPCWriter(BlockingDeque<String> ipc_queue){
-        this.ipc_queue = ipc_queue;
-    }
-}
 
 /**
  * Includes methods to read and write into the buffer (IPC pipes / console)
  * 
- * When launched as a child process, writes / reads into / from the IPC pipe. And when launched as
- * an independent process, it writes or reads from console.
+ * When launched as a child process, writes / reads into / from the IPC pipe. And when launched
+ * as an independent process, it writes or reads from console.
  * 
  * Defines different type of output streams which can be parsed by the parent.
  */
 
 public class Stream {
-    private Scanner ipipe;
-    private PrintStream opipe;
-    private BlockingDeque<String> ipc_queue;
+    private BlockingDeque<Message> oque;
+    private ConcurrentHashMap<Integer, CompletableFuture<Response>> promises;
+    private AtomicInteger id_count;
+
+    public static class Response{
+        private final int id;
+        private final String type;
+        private final String payload;
+
+        public int getId(){ return this.id; }
+        
+        public String getPayload(){ return this.payload; }
+        
+        public String getType(){ return this.type; }
+
+        public Response(String header[], String payload){
+            this.id = Integer.parseInt(header[0]);
+            this.type = header[1];
+            this.payload = payload;
+        }
+    }
+
+    private static class Message{
+        private final String msg;
+
+        String get(){ return this.msg; }
+
+        Message(int id, String type, String payload){
+            this.msg = "[" + id + "]"
+                + "[" + type.toString() + "]"            
+                + "[" + payload.length() + "]"
+                + payload;
+        }
+    }
+
+    private  static class IPCReader implements Runnable{
+        private BufferedInputStream istream;
+        private ConcurrentHashMap<Integer, CompletableFuture<Response>> promises;
+
+        @Override
+        public void run(){
+            
+            try{
+                while (true){
+                    String header[] = new String[3];
+                    StringBuilder group = new StringBuilder();
+                    int brackets = 0;
+
+                    while (brackets < 3){
+                        int chr = this.istream.read();
+
+                        if (chr == -1) throw new EOFException();
+
+                        char ch = (char) chr;
+
+                        if (ch == '[') continue;
+
+                        if (ch == ']'){
+                            header[brackets++] = group.toString();
+                            group.setLength(0);
+                        }
+                        
+                        else group.append(ch);
+                    }
+
+                    for (int i = 0; i < Integer.parseInt(header[2]); i++){
+                        int chr;
+
+                        if ((chr = this.istream.read()) == -1)
+                            throw new EOFException();
+
+                        group.append((char) chr);
+                    }
+
+                    Response res = new Response(header, group.toString());
+                    CompletableFuture<Response> promise = this.promises.remove(res.getId());
+
+                    if (promise != null) promise.complete(res);
+                }
+            } catch (IOException e){
+                // Will decide what to do here later.
+            }
+        }
+
+        IPCReader(ConcurrentHashMap<Integer, CompletableFuture<Response>> promises){
+            this.istream = new BufferedInputStream(System.in);
+            this.promises = promises;
+        }
+    }
+
+    private static class IPCWriter implements Runnable{
+        private BlockingDeque<Message> oque;
+        private BufferedOutputStream ostream;
+
+        @Override
+        public void run(){
+
+            try{
+                while (true){
+                    Message msg = oque.take();
+
+                    // write to stdout so parent/manager can read it
+                    this.ostream.write(msg.get().getBytes(StandardCharsets.UTF_8));
+                    ostream.flush();
+                }
+
+            } catch(InterruptedException e){
+                Thread.currentThread().interrupt();
+            } catch (IOException e) {
+                // Will decide what to do here later.
+            }
+        }
+
+        IPCWriter(BlockingDeque<Message> oque){
+            this.oque = oque;
+            this.ostream = new BufferedOutputStream(System.out);
+        }
+    }
 
     public enum Type{
-        LOG,
         AUTH,
+        LOG,
         CRITICAL_UPDATE,
         MINOR_UPDATE,
         PLUGIN_UPDATE
     }
 
-    public void write(Type type, String text){
-        this.opipe.println("[" + type.toString() + "]" + "[" + text.length() + "]" + text + ";");
+    /**
+     * This method is best for Fire-And-Forget kind of payloads. Best use cases are Logs, Updates,
+     * and Notifications.
+     */
+    public void send(Type type, String payload)
+    throws InterruptedException{
+        int id = this.id_count.incrementAndGet();
+        this.oque.put(new Message(id, type.toString(), payload));
     }
 
-    public String read(){ return this.ipipe.nextLine(); }
+    /**
+     * Returns CompletableFuture for the request. Adds the request made by the sub process to the
+     * queue provides promise for the response.
+     * 
+     * Best for requests which expect a response, eg., Auth.
+     */
+    public CompletableFuture<Response> request(Type type, String payload)
+    throws InterruptedException{
+        int id = this.id_count.incrementAndGet();
+
+        CompletableFuture<Response> promise = new CompletableFuture<>();
+        this.promises.put(id, promise);
+
+        this.oque.put(new Message(id, type.toString(), payload));
+
+        return promise;
+    }
     
     public Stream(){
-        this.ipipe = new Scanner(System.in);
-        this.opipe = System.out;
-        this.ipc_queue = new LinkedBlockingDeque<>();
+        this.oque = new LinkedBlockingDeque<>();
+        this.promises = new ConcurrentHashMap<>();
+        this.id_count = new AtomicInteger();
 
-        Thread writer = new Thread(new IPCWriter(this.ipc_queue));
+        Thread writer = new Thread(new IPCWriter(this.oque));
+        writer.setDaemon(true);
         writer.start();
+        
+        Thread reader = new Thread(new IPCReader(this.promises));
+        writer.setDaemon(true);
+        reader.start();
     }
 }
